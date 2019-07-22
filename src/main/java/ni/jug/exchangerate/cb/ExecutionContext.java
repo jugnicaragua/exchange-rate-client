fc9c0ee03902ffbb241d4158d977253d1dc5dc1b
Armando Alaniz <arjoalaniz@gmail.com>
2019-07-21 20:06:27 -0600

    Manejar actualizaciones concurrentes a las cookies por banco

--- src/main/java/ni/jug/exchangerate/cb/ExecutionContext.java
+++ src/main/java/ni/jug/exchangerate/cb/ExecutionContext.java
@@ -1,14 +1,10 @@
 package ni.jug.exchangerate.cb;
 
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.stream.Collectors;
 
 /**
  *
@@ -20,31 +16,30 @@ public final class ExecutionContext {
 
     private static final ExecutionContext INSTANCE = new ExecutionContext();
 
-    private final ConcurrentMap<String, Set<Cookie>> cookiesByBank;
+    private final ConcurrentMap<String, ConcurrentMap<String, String>> cookiesByBank;
 
     private ExecutionContext() {
         this.cookiesByBank = new ConcurrentHashMap<>();
     }
 
+    private ConcurrentMap<String, String> initMapIfNecessary(String bank) {
+        ConcurrentMap<String, String> cookies = cookiesByBank.get(bank);
+        if (cookies == null) {
+            cookies = new ConcurrentHashMap<>();
+            cookiesByBank.putIfAbsent(bank, cookies);
+        }
+        return cookies;
+    }
+
     public ExecutionContext addOrReplaceCookie(String bank, Cookie cookie) {
         Objects.requireNonNull(bank);
         Objects.requireNonNull(cookie);
-        Set<Cookie> cookies = cookiesByBank.get(bank);
-        if (cookies == null) {
-            cookies = new HashSet<>();
-            cookiesByBank.putIfAbsent(bank, cookies);
-        }
-        synchronized (this) {
-            cookies.add(cookie);
-        }
+        ConcurrentMap<String, String> cookies = initMapIfNecessary(bank);
+        cookies.put(cookie.getName(), cookie.getValue());
         return this;
     }
 
-    public ExecutionContext addOrReplaceCookie(String bank, String name, String value) {
-        return addOrReplaceCookie(bank, new Cookie(name, value));
-    }
-
-    public ExecutionContext addOrReplaceBdfCookies(String bank, Cookie... cookies) {
+    public ExecutionContext addOrReplaceCookies(String bank, Cookie... cookies) {
         if (!(cookies == null || cookies.length == 0)) {
             for (int i = 0; i < cookies.length; i++) {
                 addOrReplaceCookie(bank, cookies[i]);
@@ -53,11 +48,30 @@ public final class ExecutionContext {
         return this;
     }
 
+    public ExecutionContext addOrReplaceCookie(String bank, String name, String value) {
+        Objects.requireNonNull(bank);
+        Objects.requireNonNull(value);
+        ConcurrentMap<String, String> cookies = initMapIfNecessary(bank);
+        cookies.put(name, value);
+        return this;
+    }
+
+    public ExecutionContext addOrReplaceCookie(String bank, Map<String, String> cookies) {
+        if (!(cookies == null || cookies.isEmpty())) {
+            for (Map.Entry<String, String> cookie : cookies.entrySet()) {
+                addOrReplaceCookie(bank, cookie.getKey(), cookie.getValue());
+            }
+        }
+        return this;
+    }
+
     public Map<String, String> cookies(String bank) {
         Objects.requireNonNull(bank);
-        return Collections.unmodifiableMap(cookiesByBank.getOrDefault(bank, Collections.emptySet())
-                .stream()
-                .collect(Collectors.toMap(Cookie::getName, Cookie::getValue)));
+        if (cookiesByBank.containsKey(bank)) {
+            return cookiesByBank.get(bank);
+        } else {
+            return Collections.emptyMap();
+        }
     }
 
     public static ExecutionContext getInstance() {
